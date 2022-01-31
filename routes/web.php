3ae008633f2129c4628cddc72cd2bca09410d3f3
Joshua Kiya <69136659+JoshKiyakoo@users.noreply.github.com>
2022-01-31 11:07:08 +0300

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+// ======================================================================
+//             Designed by:    Eyasu Girma [Kiya]
+//                 Mobile:     +251-913-078-029
+//                 Email:      sendtokiya@gmail.com
+//                 Facebook:   https://facebook.com/JoshKiyakoo
+//                 LinkedIn:   https://linkedin.com/in/JoshKiyakoo
+//                 Twitter:    https://twitter.com/JoshKiyakoo
+//                 Telegram:   https://t.me/JoshKiyakoo
+// ======================================================================
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SubscribersDataController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/store', [SubscribersDataController::class, 'storeSubscriber'])->name('store');
